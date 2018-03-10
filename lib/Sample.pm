43d2897bcd8b385224b64814b90806bd209c5d64
Ben Kaufman <ben.whosgonna.com@gmail.com>
2018-03-10 17:37:22 -0600

    Store and retirive numbers from a DB

--- lib/Sample.pm
+++ lib/Sample.pm
@@ -1,5 +1,6 @@
 package Sample;
 use Dancer2;
+use Dancer2::Plugin::Database;
 
 our $VERSION = '0.1';
 
@@ -13,9 +14,16 @@ get '/rand/:upper_limit?' => \&random;
 sub random {
     my $max  = route_parameters->get('upper_limit') // 10;
     my $rand = 1 + int rand($max);
+    my $last = database->selectrow_hashref(
+        'SELECT * FROM numbers ORDER BY timestamp DESC LIMIT 1'
+    );
+    database->quick_insert('numbers',
+        { timestamp => time, max_num => $max, num => $rand }
+    );
     template 'rand' => {
         max        => $max,
         random_var => $rand,
+        last       => $last->{num},
         title      => 'Randomish'
     }
 }
